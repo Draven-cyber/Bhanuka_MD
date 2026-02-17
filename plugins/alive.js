26c1822dbcc8fe12f56b0035320d92e71c943aa2
bhanukamd1233-cyber <bhanukamd1233@gmail.com>
2026-02-18 01:43:57 +0530

    Update alive.js

--- plugins/alive.js
+++ plugins/alive.js
@@ -1,6 +1,5 @@
-
-const {readEnv} = require('../lib/database')
-const {cmd , commands} = require('../command')
+const { cmd, commands } = require('../command');
+const config = require('../config');
 
 cmd({
     pattern: "alive",
@@ -8,13 +7,19 @@ cmd({
     category: "main",
     filename: __filename
 },
-async(robin, mek, m,{from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply}) => {
-try{
-const config = await readEnv();
-return await robin.sendMessage(from,{image: {url: config.ALIVE_IMG},caption: config.ALIVE_MSG},{quoted: mek})
-    
-}catch(e){
-console.log(e)
-reply(`${e}`)
-}
-})
+async (danuwa, mek, m, {
+    from, quoted, body, isCmd, command, args, q, isGroup,
+    sender, senderNumber, botNumber2, botNumber, pushname,
+    isMe, isOwner, groupMetadata, groupName, participants,
+    groupAdmins, isBotAdmins, isAdmins, reply
+}) => {
+    try {
+        return await danuwa.sendMessage(from, {
+            image: { url: config.ALIVE_IMG },
+            caption: config.ALIVE_MSG
+        }, { quoted: mek });
+    } catch (e) {
+        console.log(e);
+        reply(`${e}`);
+    }
+});
